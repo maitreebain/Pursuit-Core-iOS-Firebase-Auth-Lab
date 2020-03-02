06a1a194580d20e3fe7deabd584ede2a16a320f6
maitreebain <maitreebain@gmail.com>
2020-03-02 17:24:16 -0500

    added proj

---
+++ Firebase-Auth-Lab/Firebase-Auth-Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase-Auth-Lab
+//
+//  Created by Maitree Bain on 3/2/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()]
+        
+    }
+
+
+}
+
