8afbb9192d521692c43e17be6d8da14e4e4283ae
maitreebain <maitreebain@gmail.com>
2020-03-11 07:11:46 -0400

    work on user actions

--- Firebase-Auth-Lab/Firebase-Auth-Lab/LogInViewController.swift
+++ Firebase-Auth-Lab/Firebase-Auth-Lab/LogInViewController.swift
@@ -7,24 +7,25 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LogInViewController: UIViewController {
 
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginAction(_ sender: UIButton) {
+        
     }
-    */
-
+    
+    
+    
+    @IBAction func accountButtonPressed(_ sender: UIButton) {
+    }
+    
 }
---
+++ Firebase-Auth-Lab/Pods/Target Support Files/Pods-Firebase-Auth-Lab/Pods-Firebase-Auth-Lab-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Firebase_Auth_Lab : NSObject
+@end
+@implementation PodsDummy_Pods_Firebase_Auth_Lab
+@end
